8987a1ebeeb93bb47181e4348af31c6cec54c9fb
Yann Kaiser <kaiser.yann@gmail.com>
2016-01-25 13:15:01 +0100

    Populate setup.{py,cfg}

--- setup.py
+++ setup.py
@@ -4,4 +4,27 @@ from setuptools import setup, find_packages
 setup(
     name="repeated_test",
     packages=find_packages(),
+    version="0.1a1",
+    description="A quick unittest-compatible framework for repeating a "
+                "test function over many fixtures",
+    license="MIT",
+    author="Yann Kaiser",
+    author_email="kaiser.yann@gmail.com",
+    url="https://github.com/epsy/repeated_test",
+    install_requires=['six'],
+    test_suite="repeated_test.tests",
+    keywords=['test', 'testing', 'unittest', 'fixtures'],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Topic :: Software Development :: Quality Assurance",
+        "Topic :: Software Development :: Testing",
+    ],
 )
