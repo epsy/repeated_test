9cfb30a47f0c7d0c9aca2db7f3f90ffbc9fd6108
Yann Kaiser <kaiser.yann@gmail.com>
2016-05-27 22:58:23 +0200

    Require six 1.7+
    1.6's six.with_metaclass causes a ValueError to be raised on creation

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     author_email="kaiser.yann@gmail.com",
     url="https://github.com/epsy/repeated_test",
     tests_require=['unittest2'],
-    install_requires=['six'],
+    install_requires=['six>=1.7'],
     test_suite="repeated_test.tests",
     keywords=['test', 'testing', 'unittest', 'fixtures'],
     classifiers=[
