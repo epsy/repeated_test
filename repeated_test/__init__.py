5f9e7a2a556e11753a7ce7681f3e4cb27cf134fb
Yann Kaiser <y.kaiser@criteo.com>
2022-01-15 21:05:28 -0800

    add copyright notices

--- repeated_test/__init__.py
+++ repeated_test/__init__.py
@@ -1,3 +1,7 @@
+# repeated_test -- A framework for repeating a test over many values
+# Copyright (C) 2011-2022 by Yann Kaiser and contributors. See AUTHORS and
+# COPYING for details.
+
 from repeated_test.core import Fixtures, WithTestClass
 from repeated_test.utils import tup
 
--- repeated_test/core.py
+++ repeated_test/core.py
@@ -1,3 +1,7 @@
+# repeated_test -- A framework for repeating a test over many values
+# Copyright (C) 2011-2022 by Yann Kaiser and contributors. See AUTHORS and
+# COPYING for details.
+
 import sys
 import re
 import traceback
