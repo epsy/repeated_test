3145efad9c9e4fe23d552326a125ec3e45d483ac
Yann Kaiser <y.kaiser@criteo.com>
2022-01-15 20:30:54 -0800

    allow python 3.5+

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     author_email="kaiser.yann@gmail.com",
     url="https://github.com/epsy/repeated_test",
     tests_require=[],
-    python_requires="<3.10",
+    python_requires=">=3.5",
     install_requires=['six>=1.7'],
     test_suite="repeated_test.tests",
     keywords=['test', 'testing', 'unittest', 'fixtures'],
