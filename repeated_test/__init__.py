95838e04862a446580c9d84adc21cee1df5a30e5
Yann Kaiser <kaiser.yann@gmail.com>
2022-03-20 22:16:28 -0700

    Add with-options entrypoints to top-level package

--- repeated_test/__init__.py
+++ repeated_test/__init__.py
@@ -3,8 +3,9 @@
 # COPYING for details.
 
 from repeated_test.core import Fixtures, WithTestClass
-from repeated_test.utils import tup, options
+from repeated_test.utils import tup, options, with_options, with_options_matrix
 
 __all__ = [
-    'Fixtures', 'WithTestClass', 'tup', "options"
+    'Fixtures', 'WithTestClass', 'tup',
+    "options", "with_options", "with_options_matrix",
     ]
